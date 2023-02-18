4ed5f63b1d11f0a1e0b2bd137954b5f4ccfa5fdd
Владимир <vovavasin2@mail.ru>
2023-02-18 13:01:30 +0700

    Вторая функция

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,12 @@
 function checkedLenght (a, b) {
-  return (Number(a.length) > b) ? 'Строка проходит по длине' : 'Строка не проходит по длине';
+  return (a.length > b) ? 'Строка проходит по длине' : 'Строка не проходит по длине';
 }
 checkedLenght('Some string', 4);
+
+function checkedPolindrom (string) {
+  for (let i = 0; i < string.length; i++) {
+    return (string[i] === string[string.length - 1]) ? 'Это слово полиндром' : 'Это слово не полиндром';
+  }
+}
+checkedPolindrom ('довод');
 
